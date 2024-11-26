ba278d8df07d49ae7aeaded4d38ac2d48fe92b2d
phamkimkhuong <phamkhuong436@gmail.com>
2024-11-26 09:32:39 +0700

    create project

---
+++ src/test/java/com/example/digitalcamerastore/DigitalCameraStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.digitalcamerastore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalCameraStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
