35d59e7ea8a94816503ffb1b70ef506db5480163
phamkimkhuong <phamkhuong436@gmail.com>
2024-11-28 12:13:11 +0700

    second commit

--- src/main/java/com/example/digitalcamerastore/configs/SpringSecurityConfig.java
+++ src/main/java/com/example/digitalcamerastore/configs/SpringSecurityConfig.java
@@ -6,26 +6,13 @@
 
 package com.example.digitalcamerastore.configs;
 
-/*
- * @description
- * @author: Tran Tan Dat
- * @version: 1.0
- * @created: 26-November-2024 10:30 PM
- */
-
-import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
@@ -37,41 +24,41 @@ public class SpringSecurityConfig {
     @Bean
     PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
-    }
-    //
-//	@Bean
-//	AuthenticationSuccessHandlerImpl authenticationSuccessHandlerImpl() {
-//	    return new AuthenticationSuccessHandlerImpl();
-//	  }
+//    }
+//    //
+////	@Bean
+////	AuthenticationSuccessHandlerImpl authenticationSuccessHandlerImpl() {
+////	    return new AuthenticationSuccessHandlerImpl();
+////	  }
+////
+//    @Bean
+//    SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+//        httpSecurity
+//                .csrf(csrf -> {
+//                    csrf.disable();
+//                })
+//                .authorizeHttpRequests(authorize -> {
+//                    authorize.requestMatchers("/users").authenticated();
+//                    authorize.requestMatchers("/**").permitAll();
+//                })
+//                .formLogin(form -> {
+//                    form.loginPage("/login")
+//                            .defaultSuccessUrl("/users")
+//                            .permitAll();
+////					.successHandler(authenticationSuccessHandlerImpl());
+//                })
+//                .logout(logout -> logout
+//                        .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+//                        .permitAll())
+//                .httpBasic(Customizer.withDefaults());
 //
-    @Bean
-    SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity
-                .csrf(csrf -> {
-                    csrf.disable();
-                })
-                .authorizeHttpRequests(authorize -> {
-                    authorize.requestMatchers("/users").authenticated();
-                    authorize.requestMatchers("/**").permitAll();
-                })
-                .formLogin(form -> {
-                    form.loginPage("/login")
-                            .defaultSuccessUrl("/users")
-                            .permitAll();
-//					.successHandler(authenticationSuccessHandlerImpl());
-                })
-                .logout(logout -> logout
-                        .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
-                        .permitAll())
-                .httpBasic(Customizer.withDefaults());
-
-        return httpSecurity.build();
-    }
-
-    @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailsService)
-                .passwordEncoder(passwordEncoder());
+//        return httpSecurity.build();
+//    }
+//
+//    @Autowired
+//    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+//        auth.userDetailsService(userDetailsService)
+//                .passwordEncoder(passwordEncoder());
     }
 
 }
---
+++ src/main/java/com/example/digitalcamerastore/repositories/MayAnhRepository.java
@@ -0,0 +1,17 @@
+package com.example.digitalcamerastore.repositories;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import com.example.digitalcamerastore.entities.MayAnh;
+
+@RepositoryRestResource(collectionResourceRel = "mayanhs", path = "mayanhs")
+public interface MayAnhRepository extends JpaRepository<MayAnh, Integer> {
+	@Query(value = "SELECT e FROM MayAnh e WHERE e.tenMayAnh LIKE  %:keyword%")
+	//+ " OR e.lastName LIKE %:keyword% OR e.emailAddress LIKE  %:keyword%" + " OR e.phoneNumber LIKE  %:keyword%")
+		List<MayAnh> search(@Param("keyword") String keyword);
+}
